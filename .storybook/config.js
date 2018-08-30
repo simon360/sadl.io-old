5a0b913c4f7fc40fab344cb26dad66823f987998
Simon Andrews <me@simonandrews.ca>
2018-08-30 21:18:48 +0100

    feat: introduce Source Sans Pro as the default typeface

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,14 @@
 import { configure } from "@storybook/react";
 import "normalize.css";
+import { injectGlobal } from "styled-components";
+
+injectGlobal`
+  @import url("https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700");
+
+  body {
+    font-family: "Source Sans Pro", sans-serif;
+  }
+`;
 
 // const req = require.context("../src/components", true, /\.stories\.js$/);
 
